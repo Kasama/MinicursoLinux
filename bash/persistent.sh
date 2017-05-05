4e57ddf17d38748fd0e34d51f1866177485df37f
Kasama <ralegro@bol.com.br>
2017-05-05 15:50:30 -0300

    added signal handling and some more bash stuff

---
+++ bash/persistent.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+trap "echo 'HAHA! Tentou me matar com SIGINT!'" SIGINT
+trap "echo 'HAHA! Tentou me matar com SIGTERM !'" SIGTERM
+trap "echo 'HAHA! Tentou me matar com SIGHUP!'" SIGHUP
+
+while true; do
+	echo "Ainda estou vivo!"
+	sleep 1
+done
